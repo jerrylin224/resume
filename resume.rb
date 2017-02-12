b41425331a0514a5b5934d9d4390fc9b9e7a1ef1
Mata Angelou <pk60905@hotmail.com>
2017-02-12 19:02:48 +0800

    Added pony domain

--- resume.rb
+++ resume.rb
@@ -26,17 +26,21 @@ get '/' do
 end
 
 post '/contact' do
-  from = Email.new(email: 'pk60905@gmail.com')
-  subject = 'Hello World from the SendGrid Ruby Library!'
-  to = Email.new(email: 'pk60905@gmail.com')
-  content = Content.new(type: 'text/plain', value: 'Hello, Email!')
-  mail = Mail.new(from, subject, to, content)
-
-  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-  response = sg.client.mail._('send').post(request_body: mail.to_json)
-  puts response.status_code
-  puts response.body
-  puts response.headers
+  Pony.mail(
+    from: params[:name] + '<' + params[:email] + '>',
+    to: 'pk60905@gmail.com',
+    subject: params[:name] + '想聯絡你',
+    body: '收到來自#{params[:email]} 的來信，內容是： ' + params[:message],
+    via: :smtp,
+    via_options: {
+      address:               'smtp.sendgrid.net',
+      port:                  '587',
+      enable_starttls_auto:  true,
+      user_name:             ENV['SENDGRID_USERNAME'],
+      password:              ENV['SENDGRID_PASSWORD'],
+      authentication:        :plain,
+      domain:                ENV['SENDGRID_DOMAIN']
+    }
   )
   redirect '/thank_you'
 end
