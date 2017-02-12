dc309633e9d83be291e7e040845037cb49512071
Mata Angelou <pk60905@hotmail.com>
2017-02-12 20:19:17 +0800

    Added pony domain

--- resume.rb
+++ resume.rb
@@ -30,7 +30,7 @@ post '/contact' do
     from: params[:name] + '<' + params[:email] + '>',
     to: 'pk60905@gmail.com',
     subject: params[:name] + '想聯絡你',
-    body: '收到來自#{params[:email]} 的來信，內容是： ' + params[:message],
+    body: '收到來自 ' + params[:email] + '的來信，內容是： ' + params[:message],
     via: :smtp,
     via_options: {
       address:               'smtp.sendgrid.net',
